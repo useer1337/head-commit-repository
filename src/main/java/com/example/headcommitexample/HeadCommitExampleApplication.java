eecda7ed87ec630f4b1a8d1f8c388286c5d4650e
Koval Aleksandr <a.koval@hostco.ru>
2023-12-21 16:26:52 +0500

    first commit

---
+++ src/main/java/com/example/headcommitexample/HeadCommitExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.headcommitexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadCommitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadCommitExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/headcommitexample/HeadCommitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.headcommitexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadCommitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
